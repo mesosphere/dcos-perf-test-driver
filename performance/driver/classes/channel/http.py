d26e951b015c12d996eb119ec1d71dcb983fc172
Ioannis Charalampidis <johnys2@gmail.com>
2017-05-08 13:44:54 +0200

    Keeping track of marathon deployment ID from http channel

--- performance/driver/classes/channel/http.py
+++ performance/driver/classes/channel/http.py
@@ -5,21 +5,32 @@ from performance.driver.core.template import TemplateString, TemplateDict
 from performance.driver.core.channel import Channel
 
 class HTTPRequestStartEvent(Event):
-  pass
+  def __init__(self, url, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.url = url
 
 class HTTPRequestEndEvent(Event):
-  pass
+  def __init__(self, url, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.url = url
 
 class HTTPResponseStartEvent(Event):
-  pass
+  def __init__(self, url, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.url = url
 
 class HTTPResponseEndEvent(Event):
-  pass
+  def __init__(self, url, body, headers, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.url = url
+    self.body = body
+    self.headers = headers
 
 class HTTPChannel(Channel):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self.activeRequest = None
 
     # Receive parameter updates and clean-up on teardown
     self.eventbus.subscribe(self.handleParameterUpdate, events=(ParameterUpdateEvent,))
@@ -34,6 +45,8 @@ class HTTPChannel(Channel):
     """
     Stop pending request(s) at teardown
     """
+    if self.activeRequest:
+      self.activeRequest.raw._fp.close()
 
   def handleParameterUpdate(self, event):
     """
@@ -54,7 +67,20 @@ class HTTPChannel(Channel):
     body = self.bodyTpl.apply(event.parameters)
     headers = self.headerTpl.apply(event.parameters)
 
-    # Place request
-    self.logger.debug('Placing a %s request to %s' % (verb, url))
-    request = self.session.request(verb, url, data=body, headers=headers)
+    # Helper function to notify the message bus when an HTTP response starts
+    def ack_response(request, *args, **kwargs):
+      self.eventbus.publish(HTTPRequestEndEvent(url, traceid=event.traceids))
+      self.eventbus.publish(HTTPResponseStartEvent(url, traceid=event.traceids))
 
+    # Place request
+    self.eventbus.publish(HTTPRequestStartEvent(url, traceid=event.traceids))
+    self.logger.debug('Placing a %s request to %s' % (verb, url))
+    self.activeRequest = self.session.request(verb, url,
+      data=body, headers=headers, hooks=dict(response=ack_response))
+
+    # Process response
+    self.eventbus.publish(HTTPResponseEndEvent(url, self.activeRequest.text, self.activeRequest.headers, traceid=event.traceids))
+    print(self.activeRequest.text)
+    print(self.activeRequest.headers)
+
+    self.activeRequest = None
--- performance/driver/classes/observer/marathonevents.py
+++ performance/driver/classes/observer/marathonevents.py
@@ -7,8 +7,52 @@ from contextlib import closing
 from performance.driver.core.observer import Observer
 from performance.driver.core.events import Event, LogLineEvent
 
-class MarathonStartedEvent(Event):
+from performance.driver.classes.channel.http import HTTPResponseEndEvent
+
+class MarathonEvent(Event):
   pass
+
+class MarathonStartedEvent(MarathonEvent):
+  pass
+
+class MarathonAPIEvent(Event):
+  def __init__(self, data, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.data = data
+
+class MarathonAPIPostEvent(MarathonAPIEvent):
+  """
+  """
+
+class MarathonStatusUpdateEvent(MarathonAPIEvent):
+  """
+  """
+
+class MarathonFrameworkMessageEvent(MarathonAPIEvent):
+  """
+  """
+
+class MarathonSubscribeEvent(MarathonAPIEvent):
+  """
+  """
+
+class MarathonUnsubscribeEvent(MarathonAPIEvent):
+  """
+  """
+
+class MarathonDeploymentEvent(MarathonAPIEvent):
+  """
+  High-level class from marathon deployment events
+  """
+
+MARATHON_EVENT_MAPPING = {
+  'api_post_event': MarathonAPIPostEvent,
+  'status_update_event': MarathonStatusUpdateEvent,
+  'framework_message_event': MarathonFrameworkMessageEvent,
+  'subscribe_event': MarathonSubscribeEvent,
+  'unsubscribe_event': MarathonUnsubscribeEvent,
+  'unsubscribe_event': MarathonUnsubscribeEvent,
+}
 
 class MarathonEventsObserver(Observer):
 
@@ -16,11 +60,26 @@ class MarathonEventsObserver(Observer):
     super().__init__(*args, **kwargs)
     self.url = self.getConfig('url')
     self.eventThread = None
+    self.deploymentTraceIDs = {}
 
     # Subscribe into receiving LogLine events, and place us above the
     # average priority in order to provide translated, high-level events
     # to the rest of the components
     self.eventbus.subscribe(self.handleLogLineEvent, events=(LogLineEvent,), order=2)
+
+    # When an HTTP request is completed and a marathon deployment is started,
+    # we have to keep track of the traceids of the event that initiated the
+    # request in order to trace it back to the source event.
+    self.eventbus.subscribe(self.handleHttpResponse, events=(HTTPResponseEndEvent,), order=2)
+
+  def handleHttpResponse(self, event):
+    """
+    Look for HTTP response events that contain a `Marathon-Deployment-Id` header
+    and keep track of the originating event's traceID. We keep it in order to
+    attach it on further marathon events related to the given deployment
+    """
+    if 'Marathon-Deployment-Id' in event.headers:
+      self.deploymentTraceIDs[event.headers['Marathon-Deployment-Id']] = event.traceids
 
   def handleLogLineEvent(self, event):
     """
