f9013ee15a6441f852c860ec84ac791d748586c4
Ioannis Charalampidis <johnys2@gmail.com>
2017-05-16 15:12:24 +0200

    fix(tasks.marathon): Adding support for `url` argument

--- performance/driver/classes/tasks/marathon.py
+++ performance/driver/classes/tasks/marathon.py
@@ -18,9 +18,11 @@ class RemoveAllApps(Task):
     self.logger.info('Removing all apps from marathon')
 
     # Get config parameters
-    cluster_url = self.getConfigDefinition('cluster_url', None)
+    cluster_url = self.getConfig('url', None)
     if cluster_url is None:
-      raise KeyError('Missing `cluster_url` definition')
+      cluster_url = self.getConfigDefinition('cluster_url', None)
+      if cluster_url is None:
+        raise KeyError('Missing `url` parameter or `cluster_url` definition')
 
     # Add auth headers if we have an auth_token defined
     headers = {}
@@ -32,13 +34,13 @@ class RemoveAllApps(Task):
 
     # Request list of apps
     self.logger.debug('Enumerating all apps')
-    response = requests.get('%s/service/marathon/v2/groups?_timestamp=1494604512847&embed=group.groups&embed=group.apps&embed=group.pods&embed=group.apps.deployments&embed=group.apps.counts&embed=group.apps.tasks&embed=group.apps.taskStats&embed=group.apps.lastTaskFailure' % cluster_url, verify=False, headers=headers)
+    response = requests.get('%s/v2/groups?_timestamp=1494604512847&embed=group.groups&embed=group.apps&embed=group.pods&embed=group.apps.deployments&embed=group.apps.counts&embed=group.apps.tasks&embed=group.apps.taskStats&embed=group.apps.lastTaskFailure' % cluster_url, verify=False, headers=headers)
     if response.status_code != 200:
       raise RuntimeError('Unable to enumerate running apps')
 
     # Destroy every service
     for app in response.json()['apps']:
       self.logger.debug('Removing app %s' % app['id'])
-      response = requests.delete('%s/service/marathon/v2/apps/%s' % (cluster_url, app['id']), verify=False, headers=headers)
+      response = requests.delete('%s/v2/apps/%s' % (cluster_url, app['id']), verify=False, headers=headers)
       if response.status_code != 200:
         self.logger.warn('Unable to remove app %s' % app['id'])
