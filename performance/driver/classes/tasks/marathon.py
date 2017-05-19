1482cb5e00923505d85d7a2c95d642185f8df98c
Ioannis Charalampidis <johnys2@gmail.com>
2017-05-19 10:18:55 +0200

    feat(tasks.marathon): adding RemoveGroup task

--- performance/driver/classes/tasks/marathon.py
+++ performance/driver/classes/tasks/marathon.py
@@ -10,13 +10,30 @@ from ..observer.marathonevents import MarathonDeploymentSuccessEvent
 # Disable SSL warnings
 requests.packages.urllib3.disable_warnings()
 
-class RemoveAllApps(Task):
+class MarathonDeploymentMonitorTask(Task):
   """
-  Remove all apps found in the marathon URL
+  Base class that subscribes to the event bus and waits for a success event
   """
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+
+    # Get config parameters
+    self.cluster_url = self.getConfig('url', None)
+    if self.cluster_url is None:
+      self.cluster_url = self.getConfigDefinition('cluster_url', None)
+      if self.cluster_url is None:
+        raise KeyError('Missing `url` parameter or `cluster_url` definition')
+
+    # Add auth headers if we have an auth_token defined
+    self.headers = {}
+    auth_token = self.getConfigDefinition('auth_token', None)
+    if not auth_token is None:
+      self.headers = {
+        'Authorization': 'token=%s' % auth_token
+      }
+
+    # Track delpoyments
     self.cv = threading.Condition()
     self.trackDeployments = []
     self.eventbus.subscribe(self.handleMarathonDeploymentSuccessEvent, events=(MarathonDeploymentSuccessEvent,))
@@ -26,7 +43,6 @@ class RemoveAllApps(Task):
     Keep track of completed deployments
     """
     if event.deployment in self.trackDeployments:
-      print("Removing %s from %r" % (event.deployment, self.trackDeployments))
       with self.cv:
         self.trackDeployments.remove(event.deployment)
         self.cv.notify()
@@ -41,39 +57,49 @@ class RemoveAllApps(Task):
       while len(self.trackDeployments) > 0:
         self.cv.wait()
 
+class RemoveAllApps(MarathonDeploymentMonitorTask):
+  """
+  Remove all apps found in the marathon URL
+  """
+
   def run(self):
     self.logger.info('Removing all apps from marathon')
 
-    # Get config parameters
-    cluster_url = self.getConfig('url', None)
-    if cluster_url is None:
-      cluster_url = self.getConfigDefinition('cluster_url', None)
-      if cluster_url is None:
-        raise KeyError('Missing `url` parameter or `cluster_url` definition')
-
-    # Add auth headers if we have an auth_token defined
-    headers = {}
-    auth_token = self.getConfigDefinition('auth_token', None)
-    if not auth_token is None:
-      headers = {
-        'Authorization': 'token=%s' % auth_token
-      }
-
     # Request list of apps
     self.logger.debug('Enumerating all apps')
-    response = requests.get('%s/v2/groups?_timestamp=1494604512847&embed=group.groups&embed=group.apps&embed=group.pods&embed=group.apps.deployments&embed=group.apps.counts&embed=group.apps.tasks&embed=group.apps.taskStats&embed=group.apps.lastTaskFailure' % cluster_url, verify=False, headers=headers)
+    response = requests.get('%s/v2/groups?_timestamp=1494604512847&embed=group.groups&embed=group.apps&embed=group.pods&embed=group.apps.deployments&embed=group.apps.counts&embed=group.apps.tasks&embed=group.apps.taskStats&embed=group.apps.lastTaskFailure' % self.cluster_url, verify=False, headers=self.headers)
     if response.status_code != 200:
       raise RuntimeError('Unable to enumerate running apps')
 
     # Destroy every service
     self.trackDeployments = []
     for app in response.json()['apps']:
-      self.logger.debug('Removing app %s' % app['id'])
-      response = requests.delete('%s/v2/apps/%s' % (cluster_url, app['id']), verify=False, headers=headers)
+      self.logger.info('Removing app %s' % app['id'])
+      response = requests.delete('%s/v2/apps/%s?force=true' % (self.cluster_url, app['id']), verify=False, headers=self.headers)
       if response.status_code != 200:
         self.logger.warn('Unable to remove app %s' % app['id'])
       else:
         self.trackDeployments.append(response.headers['Marathon-Deployment-Id'])
 
-    # Wait for deployments
+    # Wait for deployments to complete
+    self.waitDeployments()
+
+class RemoveGroup(MarathonDeploymentMonitorTask):
+  """
+  Removes a specific group from marathon
+  """
+
+  def run(self):
+    group_name = self.getConfig('group')
+    self.logger.info('Removing group %s from marathon' % group_name)
+
+    # Destroy group
+    self.logger.debug('Removing group %s' % group_name)
+    response = requests.delete('%s/v2/groups/%s/?force=true' % (self.cluster_url, group_name), verify=False, headers=self.headers)
+    if response.status_code != 200:
+      self.logger.warn('Unable to remove group %s' % group_name)
+    else:
+      self.trackDeployments.append(response.headers['Marathon-Deployment-Id'])
+
+    # Wait for deployments to complete
     self.waitDeployments()
