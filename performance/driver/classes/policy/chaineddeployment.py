82ebd3dabaea03beba034526977e4687e7580949
Ioannis Charalampidis <johnys2@gmail.com>
2017-06-02 12:37:11 +0300

    feat(policy.chaineddeployment): making it more configurable
    - Adding support for `waitForValueEvents` in order to wait for a   given number of events before assuming the deployment successful

--- performance/driver/classes/policy/chaineddeployment.py
+++ performance/driver/classes/policy/chaineddeployment.py
@@ -1,5 +1,5 @@
 import time
-from performance.driver.core.events import RunTaskEvent
+from performance.driver.core.events import RunTaskEvent, isEventMatching
 from performance.driver.core.classes import PolicyFSM, State
 
 class ChainedDeploymentPolicy(PolicyFSM):
@@ -9,29 +9,64 @@ class ChainedDeploymentPolicy(PolicyFSM):
     Entry point state
     """
 
+    def onEnter(self):
+      """
+      Reset state when entering the `Start` state
+      """
+
+      # Config
+      self.parameter = self.getConfig('parameter')
+      self.values = self.getConfig('values')
+      self.waitForValueEvents = self.getConfig('waitForValueEvents', False)
+      self.events = self.getConfig('events', {})
+
+      # Ensure events are in the correct format
+      if not 'success' in self.events:
+        self.events['success'] = []
+      if not type(self.events['success']) in (list, tuple):
+        self.events['success'] = [self.events['success']]
+      if not 'failure' in self.events:
+        self.events['failure'] = []
+      if not type(self.events['failure']) in (list, tuple):
+        self.events['failure'] = [self.events['failure']]
+
+      # Run-time cases
+      self.runValues = []
+      self.runEvents = 0
+      self.runSuccessful = None
+      self.traceid = None
+
     def onMarathonStartedEvent(self, event):
       """
-      When marathon has started, switch to `Run` state
+      When the tests are started we should not immediately run the tests, but
+      we should wait until marathon is ready before switching to `Run` state
       """
       self.goto(ChainedDeploymentPolicy.Run)
 
     def onRestartEvent(self, event):
       """
-      If we are instructed to restart the tests switch back to `Run` state
+      When the tests are re-started, marathon is already running, so only wait
+      for the restart signal before switching to `Run` state.
       """
       self.goto(ChainedDeploymentPolicy.Run)
 
+
   class Run(State):
     """
-    Initialize a run and start it
+    Initialize test cases and prepare for deployment
     """
 
     def onEnter(self):
       """
       Initialize test cases and start deployment
       """
-      self.cases = list(self.getConfig('steps'))
+      self.runValues = list(self.values)
+      self.runSuccessful = True
+      self.runEvents = 1
+      self.traceid = None
+
       self.goto(ChainedDeploymentPolicy.Deploy)
+
 
   class Deploy(State):
     """
@@ -39,66 +74,84 @@ class ChainedDeploymentPolicy(PolicyFSM):
     """
 
     def onEnter(self):
+      """
+      Pick next run value
+      """
 
-      # If we ran out of cases, go to sink
-      if len(self.cases) == 0:
+      # If we ran out of values, go to sink
+      if len(self.runValues) == 0:
         self.goto(ChainedDeploymentPolicy.End)
         return
 
       # Fetch the next case to process
-      case = self.cases.pop(0)
+      value = self.runValues.pop(0)
 
-      # Dispatch the request to update the test parameters. All such updates
+      # If we have to wait as many events as the value, update
+      # `runEvents` accordingly
+      if self.waitForValueEvents:
+        self.runEvents = value
+
+      # Dispatch the request to update the test parameter. All such updates
       # are batched together into a single event in the bus at the end of the
       # stack, but they will all share the same trace ID
-      self.traceid = self.setParameters(case)
+      self.traceid = self.setParameter(self.parameter, value)
 
       # We will be using the trace ID to find out which events are cascade
       # children of the initial request
 
       self.goto(ChainedDeploymentPolicy.Waiting)
 
+
   class Waiting(State):
     """
-    Waiting for deployment to complete
+    Waiting for the test to complete
     """
 
-    def onMarathonDeploymentSuccessEvent(self, event):
-      # Ignore deployment success events that do not originate from our
-      # setParameters trigger
+    def onEvent(self, event):
+      """
+      Process all relevant events
+      """
       if not event.hasTrace(self.traceid):
         return
 
-      # Successful
-      self.setStatus('OK')
+      # Check if this event is a success or a failure trigger
+      isHandled = False
+      for ev in self.events['success']:
+        if isEventMatching(event, ev):
+          isHandled = True
+          break
+      for ev in self.events['failure']:
+        if isEventMatching(event, ev):
+          self.runSuccessful = False
+          self.logger.warn('A deployment failed')
+          isHandled = True
+          break
 
-      # Run the inter-test tasks
-      self.eventbus.publish(RunTaskEvent('intertest'))
-
-    def onMarathonDeploymentFailedEvent(self, event):
-      # Ignore deployment success events that do not originate from our
-      # setParameters trigger
-      if not event.hasTrace(self.traceid):
+      # If the event is not handled, ignore it
+      if not isHandled:
         return
 
-      # Deployment failed
-      self.logger.warn('Deployment failed')
-      self.setStatus('FAILED')
-
-      # Run the inter-test tasks
-      self.eventbus.publish(RunTaskEvent('intertest'))
-
-    def onRunTaskCompletedEvent(self, event):
-      """
-      When the 'intertest' task is completed, then switch to deployment
-      """
-      if event.task != 'intertest':
+      # Check if we ran out of events that we are waiting for
+      self.runEvents -= 1
+      if self.runEvents > 0:
+        self.logger.info('Waiting for %i more events' % self.runEvents)
         return
 
-      # Schedule next deployment
-      self.goto(ChainedDeploymentPolicy.Deploy)
+      # Mark the test status
+      if self.runSuccessful:
+        self.setStatus('OK')
+      else:
+        self.setStatus('FAILED')
+
+      # Run the inter-test tasks. Upon completion this
+      self.eventbus.publish(RunTaskEvent('intertest'))
 
     def onStalledEvent(self, event):
+      """
+      If the FSM doesn't change state within a given thresshold, it's considered
+      stale and it should be reaped cleanly. This handler will mark the status
+      as "Stalled" and go to next test.
+      """
       self.logger.warn('No activity while waiting for a marathon deployment to succeed')
       self.logger.debug('This means that either marathon failed to deploy the request '
         'on time, or that you haven\'t registered an observer that emmits a '
@@ -109,6 +162,21 @@ class ChainedDeploymentPolicy(PolicyFSM):
 
       # Try next deployment
       self.goto(ChainedDeploymentPolicy.Deploy)
+
+    def onRunTaskCompletedEvent(self, event):
+      """
+      This event is received when the `intertest` task completes execution.
+      Since this task might take long time to complete we are waiting for it's
+      completion before switching to next deployment cycle.
+      """
+
+      # Ignore all other events
+      if event.task != 'intertest':
+        return
+
+      # Schedule next deployment
+      self.goto(ChainedDeploymentPolicy.Deploy)
+
 
   class End(State):
     """
