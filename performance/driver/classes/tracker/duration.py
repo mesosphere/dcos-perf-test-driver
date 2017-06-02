824c26e1dd17c6e3936cfeef76f43f26c5fb0f82
Ioannis Charalampidis <johnys2@gmail.com>
2017-06-02 12:35:33 +0300

    fix(tracker.duration): tracking edges of the tracked events

--- performance/driver/classes/tracker/duration.py
+++ performance/driver/classes/tracker/duration.py
@@ -1,45 +1,96 @@
 import time
 
 from performance.driver.core.classes import Tracker
-from performance.driver.core.events import ParameterUpdateEvent, isEventMatching
+from performance.driver.core.events import ParameterUpdateEvent, RestartEvent, TeardownEvent, isEventMatching
 
-class TrackedEvent:
-  def __init__(self, startingEvent):
-    self.event = startingEvent
+class DurationTrace:
+  def __init__(self, parameterEvent):
+    self.traceids = parameterEvent.traceids
+    self.startEvent = None
+    self.endEvent = None
+
+  def isEventTracked(self, event):
+    return event.hasTraces(self.traceids)
+
+  def start(self, event):
+    self.startEvent = event
+
+  def end(self, event):
+    self.endEvent = event
+
+  def duration(self):
+    return None
+
+  def completed(self):
+    return False
+
+class EdgeDurationTrace(DurationTrace):
+
+  def __init__(self, *args):
+    super().__init__(*args)
+    self.completedFlag = False
+
+  def end(self, event):
+    super().end(event)
+    self.completedFlag = True
+
+  def duration(self):
+    if not self.completedFlag:
+      return 0.0
+    return self.endEvent.ts - self.startEvent.ts
+
+  def completed(self):
+    return self.completedFlag
 
 class DurationTracker(Tracker):
-
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.activeTracks = []
-    self.knownTraceIDs = []
+    self.activeTraces = []
 
     self.events = self.getConfig('events')
     self.metric = self.getConfig('metric')
     self.eventbus.subscribe(self.handleEvent)
 
+  def flushTraces(self):
+    """
+    Flushes active events to the bus
+    """
+    for trace in self.activeTraces:
+      if not trace.completed():
+        self.logger.warn('Trace initiated by %r was incomplete' % trace.startEvent)
+        return
+
+      self.trackMetric(
+        self.metric,
+        trace.duration(),
+        trace.traceids
+      )
+
+    self.activeTraces = []
+
   def handleEvent(self, event):
     """
+    Handle an event from the event bus and process tracked events by calculating
+    the time of the first event and the time of the lsat event
     """
+
+    # Flush edge events when the test is completed
+    if isinstance(event, RestartEvent) or isinstance(event, TeardownEvent):
+      self.flushTraces()
 
     # Each parameter update initiates a trace of interest, therefore
     # we are keeping track of traceIDs that originate from a parameter update
     if isinstance(event, ParameterUpdateEvent):
-      self.knownTraceIDs += event.traceids
-      self.logger.debug('Keeping track of trace ID %r' % event.traceids)
+      self.flushTraces()
+      self.activeTraces.append(EdgeDurationTrace(event))
+      self.logger.info('Keeping track of trace ID %r' % event.traceids)
 
-    # Track the starting event
-    if isEventMatching(event, self.events['start']) and event.hasTraces(self.knownTraceIDs):
-      self.activeTracks.append(TrackedEvent(event))
-      self.logger.debug('Marked beginning of metric: ts=%f' % event.ts)
-
-    # If we have found an ending event, look for a matching
-    # event that continues the starting trace ID
+    # Track the `start` and `end` of events
+    if isEventMatching(event, self.events['start']):
+      for trace in self.activeTraces:
+        if trace.isEventTracked(event):
+          trace.start(event)
     elif isEventMatching(event, self.events['end']):
-      for track in self.activeTracks:
-        if track.event.hasTraces(event.traceids):
-          self.logger.debug('Marked end of metric: ts=%f' % event.ts)
-
-          duration = event.ts - track.event.ts
-          self.activeTracks.remove(track)
-          self.trackMetric(self.metric, duration, event.traceids)
+      for trace in self.activeTraces:
+        if trace.isEventTracked(event):
+          trace.end(event)
