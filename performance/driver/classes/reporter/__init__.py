f777919b7d4499179e4c8cf006ca7adc41fed3a2
Ioannis Charalampidis <johnys2@gmail.com>
2017-07-03 14:43:20 +0200

    fix(reporter.postgrest): exposing the reporter from the super module

--- performance/driver/classes/reporter/__init__.py
+++ performance/driver/classes/reporter/__init__.py
@@ -2,4 +2,5 @@ from .csv import CSVReporter
 from .raw import RawReporter
 from .datadog import DataDogReporter
 from .plot import PlotReporter
+from .postgrest import PostgRESTReporter
 from .s3 import S3Reporter
