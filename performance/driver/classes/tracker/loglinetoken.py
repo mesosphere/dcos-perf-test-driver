b5b6bac93aabf67e6611390cbd261e9727cd970f
Ioannis Charalampidis <johnys2@gmail.com>
2017-07-11 10:55:13 +0200

    fix(tracker.loglinetoken): adding type transformation functions

--- performance/driver/classes/tracker/loglinetoken.py
+++ performance/driver/classes/tracker/loglinetoken.py
@@ -6,6 +6,12 @@ from performance.driver.core.reflection import subscribesToHint, publishesHint
 
 from performance.driver.classes.observer.logline import LogLineTokenMatchEvent
 
+TYPE_TRANSFORMATIONS = {
+  'int': lambda v: int(v),
+  'float': lambda v: float(v),
+  'str': lambda v: str(v)
+}
+
 class LogLineTokenTracker(Tracker):
   """
   The *LogLine Token Tracker* is forwarding the values of the LogLine tokens
@@ -37,8 +43,20 @@ class LogLineTokenTracker(Tracker):
 
     config = self.getRenderedConfig()
     self.tokens = {}
+    self.tokenCastFn = {}
     for token in config.get('tokens', []):
       self.tokens[token['token']] = token['metric']
+      self.tokenCastFn[token['token']] = lambda v: v
+
+      # Compute the type cast function name
+      typeCastFnName = token.get('type', None)
+      if typeCastFnName:
+        if not typeCastFnName in TYPE_TRANSFORMATIONS:
+          raise ValueError('Unknown type transformation %s for token %s'
+            % (typeCastFnName, token['token']))
+
+        self.tokenCastFn[token['token']] = TYPE_TRANSFORMATIONS[typeCastFnName]
+
 
     self.eventbus.subscribe(self.handleLogLineTokenMatchEvent, events=(LogLineTokenMatchEvent,))
 
@@ -53,7 +71,7 @@ class LogLineTokenTracker(Tracker):
     # Track metric
     self.trackMetric(
       self.tokens[event.name],
-      int(event.value),
+      self.tokenCastFn[event.name](event.value),
       event.traceids
     )
 
