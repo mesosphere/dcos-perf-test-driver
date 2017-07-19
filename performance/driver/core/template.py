045325a36bcf7550e3fb74449ca9afa6498dfa86
Ioannis Charalampidis <johnys2@gmail.com>
2017-07-20 00:18:07 +0300

    fix(template): fixing regex to lazy match default

--- performance/driver/core/template.py
+++ performance/driver/core/template.py
@@ -2,7 +2,7 @@ import re
 import time
 import uuid
 
-MACRO = re.compile(r'{{(.+?)(\|.+)?}}')
+MACRO = re.compile(r'{{(.+?)(\|.+?)?}}')
 METHOD = re.compile(r'([\w_]+)\((.*)\)')
 
 class TemplateMethods:
