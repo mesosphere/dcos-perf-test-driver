16689dcb088c960007893d61639d84f5a54c4f99
Ioannis Charalampidis <johnys2@gmail.com>
2017-09-12 18:43:02 +0300

    remove(observer.httpstatus): removing unused httpstatus observer

--- performance/driver/classes/observer/__init__.py
+++ performance/driver/classes/observer/__init__.py
@@ -2,6 +2,5 @@ from .marathonevents import MarathonEventsObserver
 from .marathonlogs import MarathonLogsObserver
 from .marathonmetrics import MarathonMetricsObserver
 from .marathonpoller import MarathonPollerObserver
-from .httpstatus import HTTPStatusObserver
 from .httptiming import HTTPTimingObserver
 from .logstax import LogStaxObserver
