20e57d588ee8877a0fa9dcb78760619bb084aa1c
Ioannis Charalampidis <johnys2@gmail.com>
2017-09-20 15:17:57 +0300

    feat(reporter.s3): adding `index` support

--- performance/driver/classes/reporter/s3.py
+++ performance/driver/classes/reporter/s3.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import os
 import re
+import time
 
 from performance.driver.core.classes import Reporter
 
@@ -10,6 +11,7 @@ from performance.driver.core.classes import Reporter
 #       the logic of the file itself.
 try:
   import boto3
+  from botocore.exceptions import ClientError
 except ModuleNotFoundError:
   import logging
   logging.error('One or more libraries required by S3Reporter were not'
@@ -45,6 +47,22 @@ class S3Reporter(Reporter):
         # bucket-owner-full-control
         acl: private
 
+        # [Optional] Put the filename uploaded on the given index file
+        index:
+
+          # The path to the index JSON file
+          path: path/to/index.json
+
+          # The index entry to update
+          entry: some_key
+
+          # [Optional] How many items to keep under this key
+          max_entries: 100
+
+          # [Optional] The bucket name if different than the above
+          bucket: dcos-perf-test-results
+
+
   This reporter behaves exactly like :ref:`classref-reporter-RawReporter`, but
   the generated JSON blob is uploaded to an S3 bucket instead of a file
   in your local filesystem.
@@ -69,10 +87,11 @@ class S3Reporter(Reporter):
       aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
     # Instantiate boto
-    s3 = boto3.resource(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key)
+    s3 = boto3.client(
+      's3',
+      aws_access_key_id=aws_access_key_id,
+      aws_secret_access_key=aws_secret_access_key
+    )
 
     # Get bucket and filename
     bucket_key = config.get('path', 'results-raw.json')
@@ -82,17 +101,86 @@ class S3Reporter(Reporter):
     self.logger.info('Uploading raw results to {}'.format(bucket_key))
 
     # Upload into the bucket
-    s3.Bucket(config['bucket']) \
-      .put_object(Key=bucket_key, Body=json.dumps({
-          'time': {
-            'started': self.timeStarted,
-            'completed': datetime.datetime.now().isoformat()
-          },
-          'config': self.getRootConfig().config,
-          'raw': summarizer.raw(),
-          'sum': summarizer.sum(),
-          'indicators': summarizer.indicators(),
-          'meta': self.getMeta()
-        }, sort_keys=True, indent=2),
+    s3.put_object(Bucket=config['bucket'], Key=bucket_key, Body=json.dumps({
+        'time': {
+          'started': self.timeStarted,
+          'completed': datetime.datetime.now().isoformat()
+        },
+        'config': self.getRootConfig().config,
+        'raw': summarizer.raw(),
+        'sum': summarizer.sum(),
+        'indicators': summarizer.indicators(),
+        'meta': self.getMeta()
+      }, sort_keys=True, indent=2),
+      **optional_kwargs
+    )
+
+    # Update index if needed
+    if 'index' in config:
+      indexConfig = config['index']
+
+      # Get current value
+      indexBucket = indexConfig.get('bucket', config['bucket'])
+      indexKey = indexConfig.get('path', 'index.json')
+      index_data = {}
+      try:
+
+        # Get object
+        res = s3.get_object(
+          Bucket=indexBucket,
+          Key=indexKey
+        )
+
+        # Check response code
+        accept = True
+        if 'ResponseMetadata' in res:
+          if 'HTTPStatusCode' in res['ResponseMetadata']:
+            code = res['ResponseMetadata']['HTTPStatusCode']
+            if code < 200 or code >= 300:
+              self.logger.warn('Unable to fetch index: Server responded with {}'.format(code))
+              accept = False
+
+        # Try to parse body
+        if accept and 'Body' in res:
+          index_data = json.loads(res['Body'].read())
+
+      except json.decoder.JSONDecodeError as e:
+        self.logger.warn('Unable to parse JSON of index')
+      except ClientError as e:
+        self.logger.warn('Unable to fetch current index state: {}'.format(e))
+
+      # Check index
+      if not type(index_data) is dict:
+        self.logger.error('Unexpected index file contents. Won\'t update')
+        return
+
+      # Get/Create datasets
+      if not 'datasets' in index_data:
+        index_data['datasets'] = {}
+      dataset_data = index_data['datasets']
+
+      # Get/Create entry key
+      entry = indexConfig['entry']
+      if not entry in dataset_data:
+        dataset_data[entry] = []
+      dataset_entries = dataset_data[entry]
+
+      # Insert item
+      dataset_entries.append({
+        'ts': time.time(),
+        'meta': self.getMeta(),
+        'data': bucket_key
+      })
+
+      # Keep the most recent `max_entries`
+      dataset_entries.sort(key=lambda x: x['ts'], reverse=True)
+      if 'max_entries' in indexConfig:
+        trim_to = int(indexConfig['max_entries'])
+        dataset_entries = dataset_entries[0:trim_to]
+
+      # Update index file
+      s3.put_object(Bucket=indexBucket, Key=indexKey, Body=json.dumps(index_data, sort_keys=True, indent=2),
         **optional_kwargs
       )
+
+
