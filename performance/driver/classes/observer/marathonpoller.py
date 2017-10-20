0c53bf005d2c86a994dd81872cf048b163013e3f
Ioannis Charalampidis <johnys2@gmail.com>
2017-10-20 14:50:59 +0200

    feat(observer.marathonpoller): synthesising deployments based on /v2/groups

--- performance/driver/classes/observer/marathonpoller.py
+++ performance/driver/classes/observer/marathonpoller.py
@@ -1,17 +1,26 @@
-import threading
 import requests
 import json
 import time
 
-from .marathonevents import MarathonDeploymentSuccessEvent, MarathonStartedEvent
+from .marathonevents import *
 
 from performance.driver.core.classes import Observer
 from performance.driver.core.events import TickEvent, TeardownEvent, StartEvent
 from performance.driver.core.reflection import subscribesToHint, publishesHint
 from performance.driver.core.utils import dictDiff
 
-from performance.driver.classes.channel.http import HTTPResponseEndEvent
+from performance.driver.classes.channel.marathon import MarathonDeploymentStartedEvent
 
+DIFF_REASON_REMOVED = 0
+DIFF_REASON_CREATED = 1
+DIFF_REASON_MODIFIED = 2
+
+EMPTY_GROUP = {
+  "id": "/",
+  "apps": [],
+  "groups": [],
+  "pods": []
+}
 
 class MarathonPollerObserver(Observer):
   """
@@ -31,19 +40,34 @@ class MarathonPollerObserver(Observer):
         headers:
           Accept: test/plain
 
-  This observer is polling the ``/deployments`` endpoint 8 times per second in
-  order to detect changes in the deployments and is publishing the following
-  events:
+        # [Optional] How long to wait between consecutive polls (seconds)
+        interval: 0.5
+
+        # [Optional] How long to wait before considering the deployment "Failed"
+        # If set to 0 the deployment will never fail.
+        failureTimeout: 0
+
+        # [Optional] Event binding
+        events:
+
+          # [Optional] Which event to wait to start polling
+          start: StartEvent
+
+          # [Optional] Which event to wait to stop polling
+          stop: TeardownEvent
+
+  This observer is polling the ``/groups`` endpoint as fast as possible and it
+  calculates deferences from the previously observed state. Any differences are
+  propagated as virtual deployment events as:
 
    * ``MarathonDeploymentSuccessEvent``
    * ``MarathonDeploymentFailedEvent``
 
-  .. warning::
-     It is cruicial that a low-lattency connection is used between the test
-     driver and the marathon instance. Failing to do so a deployment might
-     appear and vanish inbetween the polling probes, failing to detect them.
+  If requested, the poller is going to look for ``MarathonDeploymentStartedEvent``
+  events and is going to wait for it to be completed in a given time. If the time
+  is passed, a synthetic failure event will be generated:
 
-     Such case will likely cause a stalled test and introduce wrong results.
+   * ``MarathonDeploymentFailedEvent``
 
   .. note::
      This observer will automatically inject an ``Authorization`` header if
@@ -54,107 +78,254 @@ class MarathonPollerObserver(Observer):
      authentication task.
   """
 
-  @subscribesToHint(HTTPResponseEndEvent, TeardownEvent, StartEvent)
+  @subscribesToHint(MarathonDeploymentStartedEvent)
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.prevDeployments = set()
-    self.deploymentTraceIDs = {}
-    self.marathonIsAlive = False
-    self.pollingActive = False
-    self.pollingThread = None
 
-    # When an HTTP request is completed and a marathon deployment is started,
-    # we have to keep track of the traceids of the event that initiated the
-    # request in order to trace it back to the source event.
-    self.eventbus.subscribe(
-        self.handleHttpResponse, events=(HTTPResponseEndEvent, ), order=2)
-
-    # Stop thread at teardown
-    self.eventbus.subscribe(self.handleTeardownEvent, events=(TeardownEvent, ))
-
-    # Start polling thread at start
-    self.eventbus.subscribe(self.handleStart, events=(StartEvent, ))
-
-  def handleStartEvent(self, event):
-    """
-    Start main thread at start
-    """
-    self.pollingActive = True
-    self.pollingThread = threading.Thread(target=self.pollingThreadTarget)
-    self.pollingThread.start()
-
-  def handleTeardownEvent(self, event):
-    """
-    Stop thread at teardown
-    """
-    self.pollingActive = False
-    self.pollingThread.join()
-
-  def pollingThreadTarget(self):
-    """
-    The main polling thread
-    """
-
-    while self.pollingActive:
-      self.checkDeployments()
-      time.sleep(0.125)
-
-  @publishesHint(MarathonStartedEvent, MarathonDeploymentSuccessEvent)
-  def checkDeployments(self):
-    """
-    Check the deployments endpoint
-    """
-
-    definitions = self.getDefinitions()
+    # Load config
     config = self.getRenderedConfig()
+    self.url = config['url']
+    self.headers = config.get('headers', {})
+    self.pollInterval = config.get('interval', 0.5)
+    self.failureTimeout = config.get('failureTimeout', 0)
 
-    url = config.get('url')
-    headers = config.get('headers', {})
+    self.requestTraceIDs = {}
+    self.requestedDeployments = set()
+    self.requestedDeploymentTimeout = {}
+    self.pollDelta = 0
+    self.connected = False
+    self.lastGroup = {}
+    self.reset()
+
+    # Keep track of outgoing deployment requests
+    self.eventbus.subscribe(self.handleRequest, events=(MarathonDeploymentStartedEvent,))
+    self.eventbus.subscribe(self.handleTick, events=(TickEvent,))
+
+  def handleTick(self, event):
+    """
+    Handle a clock tick
+    """
+    self.pollDelta += event.delta
+    if self.pollDelta >= self.pollInterval:
+      self.pollDelta = 0
+      self.pollGroupsEndpoint()
+
+  def reset(self):
+    """
+    Reset the local state
+    """
+    self.connected = False
+    self.lastGroup = {
+      "id": "/",
+      "apps": [],
+      "groups": [],
+      "pods": []
+    }
+
+  @publishesHint(MarathonDeploymentFailedEvent)
+  def failRequestedDeployment(self, inst):
+    """
+    Fail the specified requested deployment
+    """
+    self.requestedDeployments.remove(inst)
+    del self.requestedDeploymentTimeout[inst]
+    del self.requestTraceIDs[inst]
+
+    self.eventbus.publish(MarathonDeploymentFailedEvent(None, inst))
+
+  def failAllPendingRequests(self):
+    """
+    Fail all the requested deployments
+    """
+    for inst in self.requestedDeployments:
+      self.failRequestedDeployment(inst)
+
+  def failExpiredPendingRequests(self):
+    """
+    Fail all the requests that passed it's grace timeout
+    """
+    ts = time.time()
+    for inst, timeout in self.requestedDeploymentTimeout.items():
+      if timeout >= ts:
+        self.failRequestedDeployment(inst)
+
+  def handleRequest(self, event):
+    """
+    Keep track of the requested deployments
+    """
+    self.requestTraceIDs[event.instance] = event.traceids
+    self.requestedDeployments.add(event.instance)
+
+    # Set the deployment failure timeout
+    if self.failureTimeout > 0:
+      self.requestedDeploymentTimeout[event.instance] = time.time() + self.failureTimeout
+
+  @publishesHint(MarathonStartedEvent, MarathonUnavailableEvent,
+    MarathonDeploymentSuccessEvent, MarathonGroupChangeSuccessEvent)
+  def pollGroupsEndpoint(self):
+    """
+    Poll the groups endpoint
+    """
+    definitions = self.getDefinitions()
 
     # If we are missing an `Authorization` header but we have a
     # `dcos_auth_token` definition, allocate an `Authorization` header now
+    #
+    # Note: We are putting this within the loop because the `dcos_auth_token`
+    #       might appear at a later time if an authentication task is already
+    #       in progress.
+    #
+    headers = dict(self.headers)
     if not 'Authorization' in headers \
        and 'dcos_auth_token' in definitions:
       headers['Authorization'] = 'token={}'.format(
           definitions['dcos_auth_token'])
 
-    # Fetch metrics
+    # Poll the endpoint
+    group = None
     try:
-      res = requests.get(
-          '{}/v2/deployments'.format(url), headers=headers, verify=False)
-      if res.status_code != 200:
-        self.logger.debug(
-            'Deployments marathon endpoint not accessible (Received {} HTTP status code)'.
-            format(res.status_code))
-        return
+      url = '{}/v2/groups?embed=group.groups&embed=group.apps&embed=group.pods&embed=group.apps.deployments'.format(self.url)
+      self.logger.debug('Requesting {}'.format(url))
+      res = requests.get(url, headers=headers)
 
-      # Emit MarathonStartedEvent if marathon was not running yet
-      if not self.marathonIsAlive:
-        self.logger.info('Marathon web server is responding')
-        self.eventbus.publish(MarathonStartedEvent())
-        self.marathonIsAlive = True
+      # Handle HTTP response
+      if res.status_code < 200 or res.status_code >= 300:
+        self.logger.warn('Unexpected HTTP response HTTP/{}'.format(res.status_code))
+      else:
+        group = res.json()
 
-      # Emit one event for every parameter value
-      value = res.json()
-      deploymentIds = set(map(lambda x: x['id'], value))
-      for addedId in deploymentIds.difference(self.prevDeployments):
-        pass
-      for removedId in self.prevDeployments.difference(deploymentIds):
-        self.eventbus.publish(
-            MarathonDeploymentSuccessEvent(
-                removedId, {},
-                traceid=self.deploymentTraceIDs.get(removedId, None)))
-      self.prevDeployments = deploymentIds
+    except requests.ConnectionError as e:
+      pass
+    except Exception as e:
+      self.logger.error('Unexpected exception {}: {}'.format(type(e).__name__, str(e)))
 
-    except requests.exceptions.ConnectionError as e:
-      self.logger.debug('Deployments marathon endpoint not accessible')
+    # Handle connected state toggle
+    if not self.connected and group:
+      self.connected = True
+      self.lastGroup = group
+      self.logger.info('Marathon is responding')
+      self.eventbus.publish(MarathonStartedEvent())
 
-  def handleHttpResponse(self, event):
-    """
-    Look for HTTP response events that contain a `Marathon-Deployment-Id` header
-    and keep track of the originating event's traceID. We keep it in order to
-    attach it on further marathon events related to the given deployment
-    """
-    if 'Marathon-Deployment-Id' in event.headers:
-      self.deploymentTraceIDs[event.headers[
-          'Marathon-Deployment-Id']] = event.traceids
+    elif self.connected and not group:
+      self.failAllPendingRequests()
+      self.reset()
+      self.logger.warn('Marathon became unresponsive')
+      self.eventbus.publish(MarathonUnavailableEvent())
+
+    elif self.connected:
+      (diff_instances, diff_groups) = diffRootGroups(self.lastGroup, group)
+      self.lastGroup = group
+      print(diff_instances, diff_groups)
+
+      # Create one virtual deployments for every affected instance
+      for inst in diff_instances:
+        self.eventbus.publish(MarathonDeploymentSuccessEvent(None, [inst], traceid=self.requestTraceIDs.get(inst, None)))
+        if inst in self.requestedDeployments:
+          self.requestedDeployments.remove(inst)
+
+      # Create virtual group deployments
+      for grp in diff_groups:
+        self.eventbus.publish(MarathonDeploymentSuccessEvent(None, [grp], traceid=self.requestTraceIDs.get(grp, None)))
+        self.eventbus.publish(MarathonGroupChangeSuccessEvent(None, grp, traceid=self.requestTraceIDs.get(grp, None)))
+        if grp in self.requestedDeployments:
+          self.requestedDeployments.remove(grp)
+
+      # Fail expired requests
+      self.failExpiredPendingRequests()
+
+
+def diffRootGroups(group_a, group_b):
+  """
+  Calculate the differences in apps, pods and groups of the given two groups
+  """
+  diff_instances = set()
+  diff_groups = set()
+
+  # Get app IDs from two groups
+  apps_a = {}
+  for app_a in group_a['apps']:
+    apps_a[app_a['id']] = app_a
+  apps_b = {}
+  for app_b in group_b['apps']:
+    apps_b[app_b['id']] = app_b
+
+  # Check for changes in apps
+  for iid in apps_a.keys():
+    if not iid in apps_b:
+      diff_instances.add(iid) # Removed
+  for iid in apps_b.keys():
+    if not iid in apps_a:
+      if len(apps_b[iid].get('deployments', [])) == 0:
+        diff_instances.add(iid) # Added & No remaining deployments
+  for iid, app_a in apps_a.items():
+    if iid in apps_b:
+      if dictDiff(app_a, apps_b[iid]):
+        if len(apps_b[iid].get('deployments', [])) == 0:
+          diff_instances.add(iid) # Added & No remaining deployments
+
+  # Get pod IDs from two groups
+  pods_a = {}
+  for pod_a in group_a['pods']:
+    pods_a[pod_a['id']] = pod_a
+  pods_b = {}
+  for pod_b in group_b['pods']:
+    pods_b[pod_b['id']] = pod_b
+
+  # Check for changes in pods
+  for iid in pods_a.keys():
+    if not iid in pods_b:
+      diff_instances.add(iid)
+  for iid in pods_b.keys():
+    if not iid in pods_a:
+      if len(pods_b[iid].get('deployments', [])) == 0:
+        diff_instances.add(iid) # Added & No remaining deployments
+  for iid, pod_a in pods_a.items():
+    if iid in pods_b:
+      if dictDiff(pod_a, pods_b[iid]):
+        if len(pods_b[iid].get('deployments', [])) == 0:
+          diff_instances.add(iid) # Added & No remaining deployments
+
+  # Get IDs from two groups
+  groups_a = {}
+  for group_a in group_a['groups']:
+    groups_a[group_a['id']] = group_a
+  groups_b = {}
+  for group_b in group_b['groups']:
+    groups_b[group_b['id']] = group_b
+
+  # Check for changes in pods
+  for gid in groups_a.keys():
+    if not gid in groups_b:
+      diff_groups.add(gid)
+  for gid in groups_b.keys():
+    if not gid in groups_a:
+      diff_groups.add(gid)
+  for gid, pgroup_a in groups_a.items():
+    if gid in groups_b:
+      if dictDiff(pgroup_a, groups_b[gid]):
+        diff_groups.add(gid)
+
+  # For every changed group, deep into details
+  for group in diff_groups:
+    empty_group = {
+      "id": group,
+      "apps": [],
+      "pods": [],
+      "groups": []
+    }
+    if group in groups_a:
+      if group in groups_b:
+        (child_diff_instances, child_diff_groups) = diffRootGroups(groups_a[group], groups_b[group])
+        diff_instances.update(child_diff_instances)
+        diff_groups.update(child_diff_groups)
+      else:
+        (child_diff_instances, child_diff_groups) = diffRootGroups(groups_a[group], empty_group)
+        diff_instances.update(child_diff_instances)
+        diff_groups.update(child_diff_groups)
+    else:
+      (child_diff_instances, child_diff_groups) = diffRootGroups(empty_group, groups_b[group])
+      diff_instances.update(child_diff_instances)
+      diff_groups.update(child_diff_groups)
+
+  # Return instance and group diffs
+  return (diff_instances, diff_groups)
