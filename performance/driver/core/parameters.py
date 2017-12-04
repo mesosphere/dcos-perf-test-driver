25bcdf132b0fc909f33bbd8f265afede0409dce0
Ioannis Charalampidis <johnys2@gmail.com>
2017-12-04 16:01:37 +0100

    feat(core.parameters): using .flush() to submit parameters from policy

--- performance/driver/core/parameters.py
+++ performance/driver/core/parameters.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import uuid
+import queue
 
 from .events import ParameterUpdateEvent, FlagUpdateEvent
 from performance.driver.core.eventbus import EventBusSubscriber
@@ -23,9 +24,8 @@ class ParameterBatch(EventBusSubscriber):
     self.logger = logging.getLogger('ParameterBatch')
     self.config = config
     self.parameters = {}
-    self.parameterMutex = Lock()
-    self.paramUpdates = []
-    self.flagUpdates = []
+    self.paramUpdates = queue.Queue()
+    self.flagUpdates = queue.Queue()
     self.updateTraceid = uuid.uuid4().hex
     self.previousTraceId = None
 
@@ -33,60 +33,54 @@ class ParameterBatch(EventBusSubscriber):
     for key, parameter in config.parameters.items():
       self.parameters[key] = parameter['default']
 
-    # Subscribe as a last handler in the event bus
-    self.eventbus.subscribe(self.handleEvent, order=10)
-
   @publishesHint(FlagUpdateEvent, ParameterUpdateEvent)
-  def handleEvent(self, event):
+  def flush(self):
     """
+    Flush the batched `setParameter` or `setFlag` operations to the event bus.
+
     Property updates can occurr any time during an event handling process,
-    so we are waiting until the propagation has compelted (order=10) and then
-    we are collecting all the updates in a single batch.
+    so we are waiting until the policy is ready to trigger a parameter update.
     """
 
-    with self.parameterMutex:
+    # Compose a 'diff' batch and 'new' parameter space
+    batch = {}
+    parameters = dict(self.parameters)
+    while not self.paramUpdates.empty():
+      (name, value) = self.paramUpdates.get()
+      batch[name] = value
+      parameters[name] = value
 
-      # Compose a 'diff' batch and 'new' parameter space
-      batch = {}
-      parameters = dict(self.parameters)
-      for name, value in self.paramUpdates:
-        batch[name] = value
-        parameters[name] = value
+    # First dispatch flag updates for the previous run
+    while not self.flagUpdates.empty():
+      (flagName, flagValue) = self.flagUpdates.get()
+      self.eventbus.publish(
+          FlagUpdateEvent(
+              flagName, flagValue, traceid=self.previousTraceId))
 
-      # First dispatch flag updates for the previous run
-      if self.flagUpdates:
-        for flagName, flagValue in self.flagUpdates:
-          self.eventbus.publish(
-              FlagUpdateEvent(
-                  flagName, flagValue, traceid=self.previousTraceId))
+    # Then dispatch parameter updates
+    if batch:
+      self.logger.info('Setting axis to {}'.format(json.dumps(parameters)))
+      self.eventbus.publish(
+          ParameterUpdateEvent(
+              parameters, self.parameters, batch,
+              traceid=self.updateTraceid))
 
-        self.flagUpdates = []
-
-      # Then dispatch parameter updates
-      if batch:
-        self.logger.info('Setting axis to {}'.format(json.dumps(parameters)))
-        self.eventbus.publish(
-            ParameterUpdateEvent(
-                parameters, self.parameters, batch,
-                traceid=self.updateTraceid))
-
-        self.paramUpdates = []
-        self.parameters = parameters
-        self.previousTraceId = self.updateTraceid
-        self.updateTraceid = uuid.uuid4().hex
+      self.parameters = parameters
+      self.previousTraceId = self.updateTraceid
+      self.updateTraceid = uuid.uuid4().hex
 
   def setParameter(self, name, value):
     """
     Schedule a property update batch that will be triggered when the event
     handling is completed
     """
-    with self.parameterMutex:
-      self.paramUpdates.append((name, value))
+    self.logger.debug('Setting parameter {}={}'.format(name, value))
+    self.paramUpdates.put((name, value))
     return self.updateTraceid
 
   def setParameters(self, parameters):
     """
-    Schedule the update for more than one parameter simultanously
+    Schedule the update for more than one parameter simultaneously
     """
     for key, value in parameters.items():
       self.setParameter(key, value)
@@ -96,5 +90,5 @@ class ParameterBatch(EventBusSubscriber):
     """
     Set a flag for this run
     """
-    self.flagUpdates.append((flag, value))
+    self.flagUpdates.put((flag, value))
     return self.updateTraceid
